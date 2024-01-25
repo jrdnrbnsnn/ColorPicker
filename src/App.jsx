fca8ba0556d38c36ef9761aaec3c08961aeb0bfa
Jordan Robinson <jordanrobinson@Jordans-Air.lan>
2024-01-25 15:58:37 -0600

    added border

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-const Color = ({ color }) => {
-  return <div className={color}></div>;
+import { useState } from "react";
+
+const Color = ({ color, setSelectedColor, isSelected }) => {
+  return (
+    <div
+      className={isSelected ? `${color} selected` : color}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
 };
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="red" />
-        <Color color="blue" />
-        <Color color="green" />
+        <Color
+          color="red"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === "red"}
+        />
+        <Color
+          color="blue"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === "blue"}
+        />
+        <Color
+          color="green"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === "green"}
+        />
       </div>
     </div>
   );
